fd991cd4fc766506e3ffbd53c0d0e956b52d0b22
ahpzero <121105267+zeroahp@users.noreply.github.com>
2022-12-27 20:57:34 +0700

    Create Hang_So.java

---
+++ src/TITV/Hang_So.java
@@ -0,0 +1,9 @@
+package TITV;
+
+public class Hang_So {
+	public static void main(String[] args) {
+		final int x = 100; //hang so khong thay doi
+		// khi thay doi se bao loi
+		System.out.println(x);
+	}
+}
