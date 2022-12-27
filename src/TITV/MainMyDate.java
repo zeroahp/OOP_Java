6fc935b136b25c18a4d403fa6fe998bc10b9b3bd
ahpzero <121105267+zeroahp@users.noreply.github.com>
2022-12-27 20:57:29 +0700

    Create MainMyDate.java

---
+++ src/TITV/MainMyDate.java
@@ -0,0 +1,13 @@
+package TITV;
+
+public class MainMyDate {
+
+	public static void main(String[] args) {
+		MyDate md = new MyDate(20, 02, 2022);
+		md.printDay();
+		md.printMonth();
+		md.printYear();
+		md.printDayMonthYear();
+	}
+
+}
