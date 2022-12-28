16cbbde9aa35b4f2a1e9c7bdcd6a66a78e5339a4
ahpzero <121105267+zeroahp@users.noreply.github.com>
2022-12-28 13:25:45 +0700

    Create DoanThang.java

---
+++ src/buoi3/DoanThang.java
@@ -0,0 +1,14 @@
+package buoi3;
+
+import java.util.Scanner;
+
+public class DoanThang {
+	private Diem d1, d2;
+	Scanner sc = new Scanner(System.in);
+	
+	public DoanThang()
+	{
+		this.d1 = this.d2 = 0;
+	}
+	
+}
