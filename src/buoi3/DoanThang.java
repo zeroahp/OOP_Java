398efe368249749191913f31c504768ceaeb7118
ahpzero <121105267+zeroahp@users.noreply.github.com>
2023-03-19 14:36:10 +0700

    Update DoanThang.java

--- src/buoi3/DoanThang.java
+++ src/buoi3/DoanThang.java
@@ -3,76 +3,51 @@ package buoi3;
 import java.util.Scanner;
 
 import buoi2.Diem;
-
 public class DoanThang {
-	
 	private Diem d1, d2;
-	Scanner sc = new Scanner(System.in);
-	
-	public DoanThang() {
+	Scanner sc = new Scanner (System.in);
+
+	public DoanThang(){
 		d1 = new Diem();
 		d2 = new Diem();
 	}
 	
-	public DoanThang(Diem x, Diem y) {
-		d1 = new Diem(x);
-		d2 = new Diem(y);
+	public DoanThang(Diem a, Diem b){
+		d1 = new Diem(a);
+		d2 = new Diem(b);
 	}
 	
-	public DoanThang(DoanThang d3) {
-		d1 = new Diem(d3.d1);
-		d2 = new Diem(d3.d2);
+	public DoanThang(DoanThang d){
+		d1 = new Diem(d.d1);
+		d2 = new Diem(d.d2);
 	}
 	
-	public void nhap() {
-		System.out.println("Nhap toa do cho diem thu 1:");
-		d1.nhapDiem();
-		System.out.println("Nhap toa do cho diem thu 2:");
-		d2.nhapDiem();
+	public void nhap(){
+		System.out.println("Nhap diem thu nhat");
+		d1.nhap();
+		System.out.println("Nhap diem thu hai");
+		d2.nhap();
 	}
 	
-	public String toString() {
-		return "[" + d1 + "," + d2 + "]";
+	public void in(){
+		System.out.println("["+d1+","+d2+"]");
 	}
 	
-	public void tinhTien(int dx, int dy) {
-		d1.tinhTien(dx, dx);
-		d2.tinhTien(dx, dy);
+	public String toString(){
+		return "["+d1+","+d2+"]";
 	}
 	
-	public float doDai() {
-		return d1.khoangCachDiem(d2);
+	public void tinhTien(int x, int y){
+		d1.tinhTien(x, y);
+		d2.tinhTien(x, y);
 	}
 	
+	public float doDai(){
+		return d1.khoangCach(d2);
+	}
 	
-	
-	
-	
-	
-	
-	
+	public double goc(){
+		return Math.acos(Math.abs(d1.giaTriX()-d2.giaTriX())/doDai());
+	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
