89347a57e011db3f46fe32c010305745598ef856
ahpzero <121105267+zeroahp@users.noreply.github.com>
2023-03-19 14:36:04 +0700

    Update SDDoanThang.java

--- src/buoi3/SDDoanThang.java
+++ src/buoi3/SDDoanThang.java
@@ -1,28 +1,34 @@
 package buoi3;
 
-import java.util.Scanner;
 import buoi2.Diem;
 
-public class SDDoanThang {
+ class SDDoanThang {
 
 	public static void main(String[] args) {
-		Diem A = new Diem(2,5);
-		A.khoangCach();
-		Diem B = new Diem(20,35);
-		B.khoangCach();
+		Diem A = new Diem(2, 5);
+		Diem B = new Diem(20, 35);
 		
-		DoanThang AB =  new DoanThang(A,B);
-		System.out.println("Doan thang AB :" + AB.toString());
-
-		AB.doDai();
+		DoanThang AB = new DoanThang(A,B);
+		System.out.print("Doan thang AB la:");
+		AB.in();
+		
 		AB.tinhTien(5, 3);
-		System.out.println("Doan thang AB khi tinh tien doan(5,3) la: "+ AB.toString());
-
-		DoanThang CD = new DoanThang();
-		CD.nhap();
-		CD.doDai();
-		System.out.println("Do dai CD la : " +  CD.doDai());
+		System.out.println("Doang thang AB sau khi tinh tien :"+AB.toString());
 		
+		DoanThang CD = new DoanThang();
+		System.out.println("Tao doan thang CD");
+		CD.nhap();
+		System.out.print("Doan thang CD la:");
+		CD.in();
+		
+		System.out.println("Do dai CD la: "+CD.doDai());
+		
+		System.out.println("Goc CD voi truc hoanh la: "+ CD.goc());
+
+
+		
+		
+
 	}
 
 }
