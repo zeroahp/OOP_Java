7a44fb4b9c75753ec7e4a6866b39250e14c2d484
ahpzero <121105267+zeroahp@users.noreply.github.com>
2023-03-19 14:37:39 +0700

    Update SDDiem.java

--- src/buoi2/SDDiem.java
+++ src/buoi2/SDDiem.java
@@ -3,27 +3,27 @@ package buoi2;
 public class SDDiem {
 
 	public static void main(String[] args) {
-		Diem a = new Diem(3, 4);
-		System.out.print("Toa do cua diem A la : ");
-		a.hienThi();
-		
+		Diem a = new Diem(3,4);
+		System.out.print("Toa do diem a la :");
+		a.in();
 		Diem b = new Diem();
-		System.out.println("Nhap toa do cua B");
-		b.nhapDiem();
-		System.out.print("Toa do cua diem B la : ");
-		b.hienThi();
 		
+		b.nhap();
+		System.out.print("Toa do diem b la :");
+		b.in();
+		
+		//C doi xung voi B qua goc toa do
 		Diem c = new Diem(-b.giaTriX(),-b.giaTriY());
-		System.out.print("Toa do cua diem C la : ");
-		c.hienThi();
+		System.out.print("Toa do diem c la :");
+		c.in();
 		
-		System.out.println("Khoang cach tu diem B den tam O la : ");
-		System.out.println(b.khoangCach());
-		System.out.println("Khoang cach tu diem A den B la : ");
-		System.out.print(b.khoangCachDiem(a));
+		//khoang cach tu diem B den tam O
+		System.out.println("Khoang cach tu diem B den tam O la:"+b.khoangCach());
+		
+		//Khoang cach tu A den B
+		System.out.println("Khoang cach tu A den B la:"+a.khoangCach(b));
 		
 		
-
 	}
 
 }
